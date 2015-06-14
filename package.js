0b4218009d55270f551cb07d03f640088de0eee7
Netanel Gilad <netanelgilad@gmail.com>
2015-06-14 09:49:47 +0300

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'netanelgilad:angular2-typescript',
-  version: '0.0.1',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Compile Angular2 code using Typescript compiler.',
   // URL to the Git repository containing the source code for this package.
